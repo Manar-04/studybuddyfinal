3ff7ba317dfeeb8ba9778d64325baac7e85476c2
Manar-04 <163166615+Manar-04@users.noreply.github.com>
2025-12-08 15:00:57 -0500

    Add quiz button to dashboard layout

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,13 @@
   <h1>Dashboard</h1>
   <div id="deckSection"></div>
   <div id="flashcardSection"></div>
+  <div id="flashcardsList"></div>
   <div id="quizSection"></div>
+  <div id="deckSection"></div>
+<div id="flashcardSection"></div>
+<div id="quizSection">
+  <button onclick="startQuiz()">Take Quiz</button>
+</div>
   <button onclick="logout()">Logout</button>
   <script src="js/app.js"></script>
 </body>
